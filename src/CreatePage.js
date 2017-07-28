f17e532829f08da3448c080eb124b6d0e60991b8
Arthur Park <arthurh.park@gmail.com>
2017-07-27 22:16:08 -0700

    Add CreatePage

---
+++ src/CreatePage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class CreatePage extends React.Component {
+  state = {
+    description: '',
+    imageUrl: ''
+  };
+
+  render() {
+    return (
+      <div className="w-100 pa4 flex justify-center">
+        <div style={{ maxWidth: 400 }} className="">
+          <input
+            className="w-100 pa3 mv2"
+            value={this.state.description}
+            placeholder="Description"
+            onChange={e => this.setState({ description: e.target.value })}
+          />
+          <input
+            className="w-100 pa3 mv2"
+            value={this.state.imageUrl}
+            placeholder="Image Url"
+            onChange={e => this.setState({ imageUrl: e.target.value })}
+          />
+          {this.state.imageUrl &&
+            <img src={this.state.imageUrl} className="w-100 mv3" />}
+          {this.state.description &&
+            this.state.imageUrl &&
+            <button
+              className="pa3 bg-black-10 bn dim ttu pointer"
+              onClick={this._handlePost}
+            >
+              Post
+            </button>}
+        </div>
+      </div>
+    );
+  }
+
+  _handlePost = () => {
+    // ... you'll implement this in a bit
+  };
+}
+
+export default CreatePage;
