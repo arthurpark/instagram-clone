b9c6c406caed91c0a23e4ca94e7d2884c6d80aa7
Arthur Park <arthurh.park@gmail.com>
2017-07-27 22:23:03 -0700

    Add react-router

--- src/ListPage.js
+++ src/ListPage.js
@@ -4,12 +4,19 @@
  */
 import React from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
+import { Link } from 'react-router-dom';
 import Post from './Post';
 
 class ListPage extends React.Component {
   render() {
     return (
       <div className="w-100 flex justify-center">
+        <Link
+          to="/create"
+          className="fixed bg-white top-0 right-0 pa4 ttu dim black no-underline"
+        >
+          + New Post
+        </Link>
         <div className="w-100" style={{ maxWidth: 400 }}>
           {this.props.viewer.allPosts.edges.map(({ node }) =>
             <Post key={node.__id} post={node} />
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import CreatePage from './CreatePage';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route path="/" component={App} exact />
+      <Route path="/create" component={CreatePage} exact />
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
